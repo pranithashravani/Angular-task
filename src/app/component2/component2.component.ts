4603e069ae20f2d02b06d634e685edebb341e27d
pranithashravani <masamsetty.pranithashravani@gmail.com>
2019-08-08 10:23:57 +0530

    Scenario 6 in Angular Task

---
+++ src/app/component2/component2.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input,Output,EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+
+  
+    constructor() { }
+  
+    ngOnInit() {
+      }
+      @Output() public childEvent=new EventEmitter();
+ fireEvent(value){
+   this.childEvent.emit(value);
+ }
+  
+  }
+
+
